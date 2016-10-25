ee5070e282396d17ce053839ffaa1fd77a40e9d2
James Bubb <jpbubb82@gmail.com>
2016-10-25 20:31:42 +0100

    Quick database setup and first loop on home page

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
   <title>Simple PHP Blog</title>
 </head>
 <body>
-  <h1>Simple PHP Blog</h1>
+  <header>
+    <h1>Simple PHP Blog</h1>
+  </header>
+  <?php
+    // Let's connect to MySQL with PDO
+    try{
+      $dbh = new PDO('mysql:host=localhost;dbname=simple_php_blog', "root", "");
+      // Main loop to retrieve articles from database
+      foreach($dbh->query('SELECT * FROM posts') as $post){
+        print $post['article'] . "<br>";
+        print "Written on: " . $post['created_on'] . "<br>";
+        print "<hr>";
+      }
+    } catch (PDOException $e){
+      print "Error: " . $e->getMessage() . "<br>";
+      die();
+    }
+    // Done with database connection
+    $dbh = null;
+ ?>
 </body>
 </html>
